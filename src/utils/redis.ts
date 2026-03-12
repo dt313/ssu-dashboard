688ba7b8051fa33d1ed064af48c53986d3d13e3f
dt313 <dt313.dev21@gmail.com>
2026-03-13 02:18:04 +0900

    fix: redis env variable

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisUrl = process.env.NEXT_PUBLIC_REDIS_URL || 'redis://localhost:6379';
 
 interface GlobalRedis {
     redis: Redis;
